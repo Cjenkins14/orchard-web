0cd2c81f36ffb465517ab1e4e6045984c1430385
cjenkins14 <coreyjenkins24@gmail.com>
2020-06-16 12:46:06 -0500

    changing purchase screen, testing new desktop connection

--- src/Purchase/Purchase.js
+++ src/Purchase/Purchase.js
@@ -1,35 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom' 
+import './Purchase.css'
 
-
-function Buy() {
+class Buy extends Component {
+    render() {
     return (
         <div className='purchase'>
             <h1>Purchasing</h1>
-            <p>We offer two types of services. You can pick your own fruit or we can pick it for you and
-                you can pick it up at the farm.
-            </p>
-            <h2>Pricing:</h2>
-            <p>Blackberries</p>
+            <p>We allow our customers to pick their own fruit at our farm, our prices are listed below:</p>
+
+            <h2>Blackberries</h2>
             <ul>
                 <li>Half gallon: $</li>
                 <li>Gallon: $</li>
                 <li>Peck: $</li>
             </ul>
-            <p>Peaches</p>
+            <select className='buyBB' >
+                <option>1/2 Gallon</option>
+                <option>Gallon</option>
+                <option>Peck</option>
+            </select>
+            <button>
+                <Link to='/payment'>Buy Now</Link>
+            </button>
+
+            <h2>Peaches</h2>
             <ul>
                 <li>Half bushel: $</li>
                 <li>Gallon: $</li>
                 <li>Peck: $</li>
             </ul>
+            <select className='buyP'>
+                <option>1/2 Bushel</option>
+                <option>Gallon</option>
+                <option>Peck</option>
+            </select>
+            <button>
+                <Link to='/payment'>Buy Now</Link>
+            </button>
+            
             <p>Plums</p>
             <ul>
                 <li>Half bushel: $</li>
                 <li>Gallon: $</li>
                 <li>Peck: $</li>
             </ul>
-            <Link to='/payment'>Pay Here</Link>
+            <select className='buyBB'>
+                <option>1/2 Gallon</option>
+                <option>Gallon</option>
+                <option>Peck</option>
+            </select>
+            <button>
+                <Link to='/payment'>Buy Now</Link>
+            </button>
         </div>
-    )
+    )}
 }
 export default Buy;
